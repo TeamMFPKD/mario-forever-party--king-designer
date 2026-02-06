0e86c6d12e5aa122f4ccb9482d5f4fda9180ad5b
GreenSweet233 <2382028511@qq.com>
2026-02-06 23:20:48 +0800

    Add beetroot and shell hit block

--- script/level/enemy/shell_interaction_moving.gd
+++ script/level/enemy/shell_interaction_moving.gd
@@ -5,9 +5,11 @@ signal shell_harded
 @export var path_to_shell : NodePath = ".."
 @export var path_to_cast : NodePath = "../BasicShapeCast2D"
 @export var path_to_shell_status : NodePath = "../ShellStatus"
+@export var path_to_movement : NodePath = "../BasicMovement"
 
 var shell : CharacterBody2D
 var cast : ShapeCast2D
+var movement : BasicMovement
 var shell_status : ShellStatus
 var is_moving : bool = false
 
@@ -15,6 +17,7 @@ func _ready() -> void:
 	shell = get_node(path_to_shell) as CharacterBody2D
 	cast = get_node(path_to_cast) as ShapeCast2D
 	shell_status = get_node(path_to_shell_status) as ShellStatus
+	movement = get_node(path_to_movement) as BasicMovement
 
 func _physics_process(delta):
 	is_moving = moving_check()
@@ -22,7 +25,16 @@ func _physics_process(delta):
 	if not is_moving:
 		return
 
+	var original_position = cast.position
+	cast.position += Vector2(sign(movement.speed_x), sign(movement.speed_y))
 	var results = ShapeCastQuery.shape_query(shell, cast)
+	cast.position = original_position
+
+	detect_enemy(results)
+
+	detect_block(results)	
+
+func detect_enemy(results):
 	for result in results:
 		# 排除自身
 		if result == shell:
@@ -36,6 +48,17 @@ func _physics_process(delta):
 			# 撞到其他 shell
 			or (result.is_in_group("shell") and result.get_node("ShellStatus").is_moving):
 				emit_signal("shell_harded")
+
+func detect_block(results):
+	var turned : bool = false
+	for result in results:
+		if !result.has_meta("interaction_with_block"):
+			continue
+		var block_hit_node = result.get_meta("interaction_with_block") as BlockHit
+		block_hit_node.on_block_hit(shell)
+		if not turned and not shell.is_on_wall():
+			turned = true
+			movement.speed_x *= -1
 
 func moving_check() -> bool:
 	return shell_status.is_moving
--- script/level/projectile/beetroot_interaction.gd
+++ script/level/projectile/beetroot_interaction.gd
@@ -5,9 +5,26 @@ signal player_sound_bump
 
 @export var beetroot : CharacterBody2D
 @export var cast : ShapeCast2D
+@export var path_to_movement : NodePath = "../BeetrootMovement"
+
+var movement : BasicMovement
+
+var interacting_blocks : Array[Node]
+
+func _ready():
+	movement = get_node(path_to_movement)
 
 func _physics_process(delta):
+	var original_position = cast.position
+	cast.position += Vector2(sign(movement.speed_x), sign(movement.speed_y))
 	var results = ShapeCastQuery.shape_query(beetroot, cast)
+	cast.position = original_position
+
+	detect_enemies(results)
+	
+	detect_blocks(results)
+
+func detect_enemies(results):
 	for result in results:
 		if !result.has_meta("interaction_with_beetroot"):
 			continue
@@ -19,3 +36,14 @@ func _physics_process(delta):
 			emit_signal("player_sound_bump")
 		if interaction_with_beetroot_node.beetroot_bounce:
 			emit_signal("beetroot_bounce")
+
+func detect_blocks(results):
+	for result in results:
+		if !result.has_meta("interaction_with_block"):
+			continue
+		var block_hit_node = result.get_meta("interaction_with_block") as BlockHit
+		block_hit_node.on_block_hit(beetroot)
+		if not result in interacting_blocks:
+			interacting_blocks.append(result)
+			emit_signal("beetroot_bounce")
+		
