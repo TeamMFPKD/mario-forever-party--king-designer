fc628532598621b65ce28bb9be05f4026c0caa13
GreenSweet233 <2382028511@qq.com>
2026-02-10 18:51:40 +0800

    Add players get into edit together

--- script/title/game_start.gd
+++ script/title/game_start.gd
@@ -1,7 +1,21 @@
 extends Node
 
-@export var jump_to_scene_node : Node
+signal game_started
 
-func _on_game_start() -> void:
-	jump_to_scene_node.jump_to_scene()
+var multiplayer_manager : MultiplayerManager
+
+func _ready() -> void:
+	multiplayer_manager = get_tree().get_first_node_in_group("multiplayer_manager") as MultiplayerManager
+
+func _on_start_button_pressed() -> void:
+	if not multiplayer_manager.multiplayer.is_server():
+		return
+	game_start.rpc()
 	
+func on_game_start() -> void:
+	emit_signal("game_started")
+
+@rpc("authority", "call_local")
+func game_start() -> void:
+	print("要开始了哟~")
+	on_game_start()
--- script/title/jump_to_scene.gd
+++ script/title/jump_to_scene.gd
@@ -3,5 +3,5 @@ extends Node
 @export var scene_uid : StringName
 
 func jump_to_scene() -> void:
-    get_tree().change_scene(scene_uid)
+	get_tree().change_scene_to_file(scene_uid)
 	
