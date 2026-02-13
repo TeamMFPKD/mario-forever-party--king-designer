db1e5ccf305d5725c2f6f419697a979f721d0cde
GreenSweet233 <2382028511@qq.com>
2026-02-13 20:44:32 +0800

    Add i18n option save

--- script/language_button.gd
+++ script/language_button.gd
@@ -1,10 +1,22 @@
 extends Button
 
+var config : ConfigFile
+
 func _ready():
 	# 设置按钮初始文本
 	update_button_text()
 	# 连接按钮点击信号
 	pressed.connect(_on_pressed)
+
+	config = GameConfig.config
+
+	var language = config.get_value("options", "language", "en")
+	match language:
+		"en":
+			TranslationServer.set_locale("en")
+		"zh":
+			TranslationServer.set_locale("zh")
+
 
 func _on_pressed():
 	# 获取当前语言
@@ -13,11 +25,15 @@ func _on_pressed():
 	# 切换语言
 	if current == "zh":
 		TranslationServer.set_locale("en")
+		config.set_value("options", "language", "en")
 	else:
 		TranslationServer.set_locale("zh")
+		config.set_value("options", "language", "zh")
 	
 	# 更新按钮上的文字
 	update_button_text()
+
+	GameConfig.save()
 
 func update_button_text():
 	# 按钮上显示当前语言的另一选项
