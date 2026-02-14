005bfd9dd5481c60791d234edd0686c4d6de268b
GreenSweet233 <2382028511@qq.com>
2026-02-14 17:19:55 +0800

    Hide item group if there is an extra click

--- script/edit/group_button.gd
+++ script/edit/group_button.gd
@@ -5,16 +5,21 @@ extends Button
 func _ready() -> void:
 	pressed.connect(_on_button_pressed)
 	if not item_group:
-		print("Warning: item_group is not assigned in ItemButton")
+		push_error("item_group is not assigned in ItemButton")
 	else:
 		item_group.visible = false
 
 func _on_button_pressed() -> void:
 	if item_group:
+		if item_group.visible:
+			item_group.visible = false
+			return
 		var item_groups = get_tree().get_nodes_in_group("item_group")
 		for node in item_groups:
 			if node is Control:
 				var control = node as Control
 				control.visible = false
 		item_group.visible = true
+	else:
+		push_error("item_group is not assigned in ItemButton")
 	
