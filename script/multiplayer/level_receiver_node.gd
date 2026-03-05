1c86d75e71e785633c10fd6b579f2067b4d0a19b
GreenSweet233 <2382028511@qq.com>
2026-03-05 20:22:49 +0800

    Add level cache preview

--- script/multiplayer/level_receiver_node.gd
+++ script/multiplayer/level_receiver_node.gd
@@ -23,3 +23,4 @@ func _process(_delta):
 			print("[%s] 玩家 " % Time.get_time_string_from_system(), player.name, " 的关卡 ", player.level_file_name, " 的数据：", player.level_data)
 			file.close()
 			print("[%s] 玩家 " % Time.get_time_string_from_system(), player.name, " 的关卡数据已缓存到本地")
+			print("[%s] 关卡预览：" % Time.get_time_string_from_system(), player.level_data)
