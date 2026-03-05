22ff319471e7980238b436f13b655e78943cb6ba
GreenSweet233 <2382028511@qq.com>
2026-03-05 19:38:41 +0800

    Add time to level cache

--- script/multiplayer/level_receiver_node.gd
+++ script/multiplayer/level_receiver_node.gd
@@ -20,6 +20,6 @@ func _process(_delta):
 			# 缓存非空关卡数据
 			var file = FileAccess.open(player.level_file_name, FileAccess.WRITE)
 			file.store_string(player.level_data)
-			print("玩家 ", player.name, " 的关卡 ", player.level_file_name, " 的数据：", player.level_data)
+			print("[%s] 玩家 " % Time.get_time_string_from_system(), player.name, " 的关卡 ", player.level_file_name, " 的数据：", player.level_data)
 			file.close()
-			print("玩家 ", player.name, " 的关卡数据已缓存到本地")
+			print("[%s] 玩家 " % Time.get_time_string_from_system(), player.name, " 的关卡数据已缓存到本地")
