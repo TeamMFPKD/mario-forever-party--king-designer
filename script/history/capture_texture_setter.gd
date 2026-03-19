5b400e2fa133e92b0761745ec1cae4c20353cdcb
GreenSweet233 <2382028511@qq.com>
2026-03-19 22:17:29 +0800

    Optimize liked courses load

--- script/history/capture_texture_setter.gd
+++ script/history/capture_texture_setter.gd
@@ -7,21 +7,25 @@ func set_photo(lvl_file_name: String) -> void:
 	if not texture_rect:
 		push_error("CaptureTextureSetter: parent is not a TextureRect")
 		return
-	_load_and_display(texture_rect, lvl_file_name)
 
-
-func _load_and_display(texture_rect: TextureRect, lvl_file_name: String) -> void:
 	var base_name := lvl_file_name.get_file().get_basename()
 	var photo_path := "user://" + LIKED_COURSE_FOLDER_NAME + "/" + base_name + ".png"
 
-	if not FileAccess.file_exists(photo_path):
-		push_warning("CaptureTextureSetter: screenshot not found: %s" % photo_path)
-		return
+	WorkerThreadPool.add_task(func():
+		if not FileAccess.file_exists(photo_path):
+			push_warning("CaptureTextureSetter: screenshot not found: %s" % photo_path)
+			return
 
-	var image := Image.new()
-	if image.load(photo_path) != OK:
-		push_error("CaptureTextureSetter: failed to load image: %s" % photo_path)
-		return
+		var image := Image.new()
+		if image.load(photo_path) != OK:
+			push_error("CaptureTextureSetter: failed to load image: %s" % photo_path)
+			return
 
-	image.resize(320, 240, Image.INTERPOLATE_LANCZOS)
-	texture_rect.texture = ImageTexture.create_from_image(image)
+		image.resize(320, 240, Image.INTERPOLATE_LANCZOS)
+		call_deferred("_apply_texture", image)
+	)
+
+func _apply_texture(image: Image) -> void:
+	var texture_rect := get_parent() as TextureRect
+	if texture_rect:
+		texture_rect.texture = ImageTexture.create_from_image(image)
