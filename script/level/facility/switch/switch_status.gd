a277ab3fc5f21ca68a86fec492a15efdd068505c
GreenSweet233 <2382028511@qq.com>
2026-03-20 13:19:52 +0800

    Switch can be triggered only once per frame

--- script/level/facility/switch/switch_status.gd
+++ script/level/facility/switch/switch_status.gd
@@ -1,13 +1,17 @@
 class_name SwitchStatus
-
 extends Node
 
 signal switched
 signal changed_to_on
 signal changed_to_off
 
-@export var is_on : bool = true:
+var _switched_this_physics_frame: bool = false
+
+@export var is_on: bool = true:
 	set(value):
+		if _switched_this_physics_frame:
+			return
+		_switched_this_physics_frame = true
 		is_on = value
 		if is_on:
 			emit_signal("changed_to_on")
@@ -16,3 +20,7 @@ signal changed_to_off
 		emit_signal("switched")
 	get():
 		return is_on
+
+func _physics_process(_delta: float) -> void:
+	_switched_this_physics_frame = false
+	
