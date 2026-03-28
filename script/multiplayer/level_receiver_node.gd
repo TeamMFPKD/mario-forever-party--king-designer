8765218fcc6f9b03b63008493e2f05d7d94f6e8b
GreenSweet233 <2382028511@qq.com>
2026-03-28 17:27:33 +0800

    Fix host's unfinished level data sync

--- script/multiplayer/level_receiver_node.gd
+++ script/multiplayer/level_receiver_node.gd
@@ -15,12 +15,11 @@ func _process(_delta):
 	for player in multiplayer_manager.players:
 		if saved_player_level_data.has(player):
 			continue
-		if player.level_file_name != "invalid":
-			saved_player_level_data.append(player)
-			# 缓存非空关卡数据
-			var file = FileAccess.open(player.level_file_name, FileAccess.WRITE)
-			file.store_string(player.level_data)
-			print("[%s] 玩家 " % Time.get_time_string_from_system(), player.name, " 的关卡 ", player.level_file_name, " 的数据：", player.level_data)
-			file.close()
-			print("[%s] 玩家 " % Time.get_time_string_from_system(), player.name, " 的关卡数据已缓存到本地")
-			print("[%s] 关卡预览：" % Time.get_time_string_from_system(), player.level_data)
+		saved_player_level_data.append(player)
+		# 缓存非空关卡数据
+		var file = FileAccess.open(player.level_file_name, FileAccess.WRITE)
+		file.store_string(player.level_data)
+		print("[%s] 玩家 " % Time.get_time_string_from_system(), player.name, " 的关卡 ", player.level_file_name, " 的数据：", player.level_data)
+		file.close()
+		print("[%s] 玩家 " % Time.get_time_string_from_system(), player.name, " 的关卡数据已缓存到本地")
+		print("[%s] 关卡预览：" % Time.get_time_string_from_system(), player.level_data)
