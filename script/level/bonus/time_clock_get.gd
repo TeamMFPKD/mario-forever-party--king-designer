5bc8dd6f5fa1255cb8782162f921acf2e460ef34
GreenSweet233 <2382028511@qq.com>
2026-04-11 17:14:54 +0800

    Add time clock and edit-game time HUD

---
+++ script/level/bonus/time_clock_get.gd
@@ -0,0 +1,30 @@
+extends Node
+
+signal time_clock_got
+
+@export var time_to_increase : float = 3.0
+@export var ani : Node2D
+
+var game_timer : Timer
+var is_activated : bool = false
+
+func _ready() -> void:
+	game_timer = get_tree().get_first_node_in_group("game_timer")
+
+func _on_time_clock_get() -> void:
+	print("Player entered time clock.")
+	if is_activated:
+		return
+	print("Player entered not activated time clock.")
+	if not game_timer:
+		push_error("game_timer not found")
+		return
+	print("Time should be increased.")
+	is_activated = true
+	ani.visible = false
+	emit_signal("time_clock_got")
+	game_timer.wait_time = game_timer.time_left + time_to_increase
+	game_timer.start()
+
+func _on_all_process_finished() -> void:
+	get_parent().queue_free()
---
+++ script/multiplayer/test_time_label.gd
@@ -0,0 +1,16 @@
+extends Label
+
+var timer : Timer
+
+func _ready() -> void:
+	visible = false
+	timer = get_tree().get_first_node_in_group("game_timer")
+	if GameModeSingleton.game_mode == GameModeSingleton.GameModeType.TEST:
+		visible = true
+
+func _process(delta: float) -> void:
+	if GameModeSingleton.game_mode != GameModeSingleton.GameModeType.TEST:
+		return
+	if not timer:
+		return
+	text = str(int(ceil(timer.time_left)))
