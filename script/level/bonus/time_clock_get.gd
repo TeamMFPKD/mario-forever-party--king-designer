fc2252e0d7609e6916b73a7aa6ab463770b4cd75
GreenSweet233 <2382028511@qq.com>
2026-04-12 14:05:11 +0800

    Add clock time changed hint label

--- script/level/bonus/time_clock_get.gd
+++ script/level/bonus/time_clock_get.gd
@@ -5,26 +5,41 @@ signal time_clock_got
 @export var time_to_increase : float = 3.0
 @export var ani : Node2D
 
+@export var time_clock_label_scene : PackedScene = preload("uid://du6kfdkecp8h0")
+@export var add_time_color : Color = Color(0.25, 1.0, 0.25)
+@export var sub_time_color : Color = Color(1.0, 0.25, 0.25)
+
 var game_timer : Timer
 var is_activated : bool = false
+var parent : Node2D
 
 func _ready() -> void:
 	game_timer = get_tree().get_first_node_in_group("game_timer")
+	parent = get_parent()
 
 func _on_time_clock_get() -> void:
-	print("Player entered time clock.")
 	if is_activated:
 		return
-	print("Player entered not activated time clock.")
 	if not game_timer:
 		push_error("game_timer not found")
 		return
-	print("Time should be increased.")
 	is_activated = true
 	ani.visible = false
 	emit_signal("time_clock_got")
 	game_timer.wait_time = clampf(game_timer.time_left + time_to_increase, 0.02, 999.0)
 	game_timer.start()
+	create_time_clock_label()
 
 func _on_all_process_finished() -> void:
-	get_parent().queue_free()
+	parent.queue_free()
+
+func create_time_clock_label() -> void:
+	var time_clock_label_node = time_clock_label_scene.instantiate() as Node2D
+	var time_clock_label = time_clock_label_node.get_node("TimeClockLabel") as Label
+	time_clock_label_node.position = parent.position
+	time_clock_label.text = ("+" if time_to_increase >= 0 else "") + str(int(time_to_increase)) + "s"
+	if time_to_increase >= 0:
+		time_clock_label.self_modulate = add_time_color
+	else:
+		time_clock_label.self_modulate = sub_time_color
+	parent.add_sibling(time_clock_label_node)
