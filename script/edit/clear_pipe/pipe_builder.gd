60a8ac90c7208ad0b066661e10c826abe15dbfc6
GreenSweet233 <2382028511@qq.com>
2026-04-18 23:14:21 +0800

    Finally pipe corners are correct

--- script/edit/clear_pipe/pipe_builder.gd
+++ script/edit/clear_pipe/pipe_builder.gd
@@ -113,6 +113,14 @@ func build_pipes() -> void:
 					"type": "straight"
 				})
 	
+	# 放置拐角零件
+	for corner_idx in corner_indices:
+		var corner_pos = pts[corner_idx]
+		var prev_direction = get_orthogonal_direction(pts[corner_idx-1], pts[corner_idx])
+		var next_direction = get_orthogonal_direction(pts[corner_idx], pts[corner_idx+1])
+		place_corner(corner_pos, prev_direction, next_direction)
+		print("  生成拐角: 位置(%.0f,%.0f), 从%s到%s" % [corner_pos.x, corner_pos.y, dir_to_str(prev_direction), dir_to_str(next_direction)])
+
 	# 如果没有任何直线段，退出
 	if fixed_straights.is_empty():
 		print("没有直线段可生成")
@@ -181,6 +189,42 @@ func place_cap(center: Vector2, outward_dir: Vector2) -> void:
 	elif outward_dir == Vector2.DOWN:
 		scene = cap_down_scene
 	else: return
+	if scene:
+		var inst = scene.instantiate()
+		inst.position = center
+		inst.rotation_degrees = rot
+		add_child(inst)
+
+
+func place_corner(center: Vector2, incoming_dir: Vector2, outgoing_dir: Vector2) -> void:
+	var scene: PackedScene
+	var rot: float = 0.0
+	
+	print("    正在放置拐角: 位置=(%.0f,%.0f), 从%s到%s" % [center.x, center.y, dir_to_str(incoming_dir), dir_to_str(outgoing_dir)])
+	# 根据拐角点周围的相邻点位置来确定拐角类型
+	# incoming_dir是管道进入的方向，outgoing_dir是管道出去的方向
+	# 所以在拐角点，incoming_dir的反方向和outgoing_dir方向都有管道
+	var left_has_pipe = (incoming_dir == Vector2.RIGHT || outgoing_dir == Vector2.LEFT)
+	var right_has_pipe = (incoming_dir == Vector2.LEFT || outgoing_dir == Vector2.RIGHT)
+	var up_has_pipe = (incoming_dir == Vector2.DOWN || outgoing_dir == Vector2.UP)
+	var down_has_pipe = (incoming_dir == Vector2.UP || outgoing_dir == Vector2.DOWN)
+	
+	if up_has_pipe && right_has_pipe:  # 上方和右侧有管道
+		scene = corner_ur_scene
+		print("    选择UR拐角: 上方和右侧有管道")
+	elif up_has_pipe && left_has_pipe:  # 上方和左侧有管道
+		scene = corner_ul_scene
+		print("    选择UL拐角: 上方和左侧有管道")
+	elif down_has_pipe && right_has_pipe:  # 下方和右侧有管道
+		scene = corner_dr_scene
+		print("    选择DR拐角: 下方和右侧有管道")
+	elif down_has_pipe && left_has_pipe:  # 下方和左侧有管道
+		scene = corner_dl_scene
+		print("    选择DL拐角: 下方和左侧有管道")
+	else:
+		print("    无法识别的管道配置: left=%s, right=%s, up=%s, down=%s" % [left_has_pipe, right_has_pipe, up_has_pipe, down_has_pipe])
+		return
+	
 	if scene:
 		var inst = scene.instantiate()
 		inst.position = center
