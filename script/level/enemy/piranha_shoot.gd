b0ccd02949803634d76338aa603c17ca7b913a35
GreenSweet233 <2382028511@qq.com>
2026-04-28 22:06:27 +0800

    Fix fireball piranha player ref

--- script/level/enemy/piranha_shoot.gd
+++ script/level/enemy/piranha_shoot.gd
@@ -18,6 +18,11 @@ func _ready():
 func _on_shoot():
 	var fireball = piranha_fireball_scene.instantiate() as Node2D
 	fireball.position = shoot_position_marker.global_position
+	if not player:
+		player = get_tree().get_first_node_in_group("player") as Node2D
+		if not player:
+			push_error("Piranha shoot: Player not found.")
+			return
 	fireball.set_meta("fireball_direction", 1 if parent.global_position.x < player.global_position.x else -1)
 	parent.add_sibling(fireball)
 	emit_signal("play_sound_shoot")
