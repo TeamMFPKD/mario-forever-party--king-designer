6fa22419c6fd0a1a658f25a1f86a35fe3a1bd998
GreenSweet233 <2382028511@qq.com>
2026-05-30 23:37:53 +0800

    MSG file name accuracy => seconds'

--- script/multiplayer/message_system/message_cache.gd
+++ script/multiplayer/message_system/message_cache.gd
@@ -1,8 +1,7 @@
 extends Node
 
 var multiplayer_manager: MultiplayerManager
-var _file_path: String
-var _ready_to_save: bool = false
+var _file_path: String = ""
 
 func _ready() -> void:
 	multiplayer_manager = get_tree().get_first_node_in_group("multiplayer_manager") as MultiplayerManager
@@ -10,8 +9,20 @@ func _ready() -> void:
 		push_error("MessageCache: MultiplayerManager not found")
 		return
 
+	multiplayer_manager.messages_updated.connect(_on_messages_updated)
+	multiplayer_manager.players_updated.connect(_on_players_updated)
+	if multiplayer_manager.players.size() >= 2:
+		_init_file_path()
+
+func _on_players_updated() -> void:
+	if _file_path == "" and multiplayer_manager.players.size() >= 2:
+		_init_file_path()
+	elif _file_path != "" and multiplayer_manager.players.size() < 2:
+		_file_path = ""
+
+func _init_file_path() -> void:
 	var now = Time.get_datetime_dict_from_system()
-	var time_str = "%04d-%02d-%02d_%02d-%02d" % [now.year, now.month, now.day, now.hour, now.minute]
+	var time_str = "%04d-%02d-%02d_%02d-%02d-%02d" % [now.year, now.month, now.day, now.hour, now.minute, now.second]
 
 	var dir = DirAccess.open("user://")
 	if not dir.dir_exists("messages"):
@@ -19,14 +30,11 @@ func _ready() -> void:
 
 	_file_path = "user://messages/%s_messages.txt" % time_str
 
-	multiplayer_manager.messages_updated.connect(_on_messages_updated)
-	_ready_to_save = true
-
 	if multiplayer_manager.messages.size() > 0:
 		_save()
 
 func _on_messages_updated() -> void:
-	if not _ready_to_save:
+	if _file_path == "":
 		return
 	_save()
 
