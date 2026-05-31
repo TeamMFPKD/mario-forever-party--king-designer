575d774b0ce05453a1042ba37ef1e046efc446f1
GreenSweet233 <2382028511@qq.com>
2026-05-31 11:15:27 +0800

    Tidy up files; Add pink coin SE

--- script/multiplayer/message_system/message_sender.gd
+++ script/multiplayer/message_system/message_sender.gd
@@ -17,9 +17,10 @@ func _on_line_edit_text_changed(new_text: String) -> void:
 		send_button.set_disabled(false)
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and event.keycode == KEY_ENTER:
-		if not send_button.is_disabled() and send_button.global_position.x > 0.0:
-			send_message()
+	if event is InputEventKey and event.pressed:
+		if event.keycode == KEY_ENTER or event.keycode == KEY_KP_ENTER:
+			if not send_button.is_disabled() and send_button.global_position.x > 0.0:
+				send_message()
 
 func _on_send_button_pressed():
 	send_message()
