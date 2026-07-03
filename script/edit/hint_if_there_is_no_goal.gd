41a423960acde8319da829d7a73d3558d23d3cd5
GreenSweet233 <139688734+GreenSweet233@users.noreply.github.com>
2026-07-03 10:38:45 +0800

    Optimize mouse popup detection

--- script/edit/hint_if_there_is_no_goal.gd
+++ script/edit/hint_if_there_is_no_goal.gd
@@ -1,24 +1,23 @@
 extends Node
 
-@export var ani_player: AnimationPlayer
-@export var hint_label: Label
+signal goal_exists
+signal none_goal_hint
 
 var timer: Timer
+
 
 func _ready() -> void:
 	timer = get_tree().get_first_node_in_group("timer_singleton") as Timer
 
+
 func _process(_delta: float) -> void:
-	if not hint_label:
-		push_error("hint_label is not set")
-		return
-	hint_label.visible = false
 	if ceil(timer.time_left) > 10:
 		return
+
 	var goals = get_tree().get_nodes_in_group("spawner_goal_gate")
+
 	if goals.size() == 0:
-		if not ani_player:
-			push_error("ani_player is not set")
-			return
-		hint_label.visible = true
-		ani_player.play(&"new_animation")
+		none_goal_hint.emit()
+	else:
+		goal_exists.emit()
+		
--- script/edit/mouse_pop.gd
+++ script/edit/mouse_pop.gd
@@ -11,6 +11,8 @@ var _origin_offset_bottom: float
 var _target_offset: float
 var _should_pop: bool = false
 
+@export var group_panels: Array[Control]
+
 func _ready() -> void:
 	_origin_offset_top = bottom_bar.offset_top
 	_origin_offset_bottom = bottom_bar.offset_bottom
@@ -38,7 +40,7 @@ func _update_target() -> void:
 	var bar_top_y = 1080.0 + bottom_bar.offset_top
 	var viewport_bottom = sub_viewport_container.offset_bottom
 
-	if mouse_local_y >= bar_top_y - trigger_distance:
+	if mouse_local_y >= bar_top_y - trigger_distance and are_all_buttons_invisible():
 		_target_offset = viewport_bottom - 1080.0 - _origin_offset_top + 32.0
 	else:
 		_target_offset = 0.0
@@ -50,3 +52,9 @@ func _move_toward_target(delta: float) -> void:
 	var new_offset = move_toward(current_offset, _target_offset, pop_speed * delta)
 	bottom_bar.offset_top = _origin_offset_top + new_offset
 	bottom_bar.offset_bottom = _origin_offset_bottom + new_offset
+
+func are_all_buttons_invisible() -> bool:
+	for panel in group_panels:
+		if panel.visible:
+			return false
+	return true
